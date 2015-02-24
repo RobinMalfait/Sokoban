0c4599002199fbf19a47ce8294a645c7d04c2ca4
Robin Malfait <malfait.robin@gmail.com>
2015-02-24 08:35:46 +0100

    Cleaning unused imports.

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -1,13 +1,8 @@
 package persistentie;
 
 import domein.Speler;
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
