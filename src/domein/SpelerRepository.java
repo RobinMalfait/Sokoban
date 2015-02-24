a311430a3c4525693a3052794e4e85283b15d9c9
Demian Dekoninck <demian@live.be>
2015-02-24 09:21:24 +0100

    meldAan method edited, SpelerRepository edited (Constructor, meldAan, ...)

--- src/domein/SpelerRepository.java
+++ src/domein/SpelerRepository.java
@@ -1,14 +1,30 @@
 package domein;
 
 import java.util.*;
+import persistentie.SpelerMapper;
 
 public class SpelerRepository
 {
-
+    private final SpelerMapper Spelermapper; 
     private List<Speler> spelers;
 
+    public SpelerRepository()
+    {
+        this.Spelermapper = new SpelerMapper();
+    }
+    
     /**
-     *
+     * 
+     * @param gebruikersnaam
+     * @param wachtwoord
+     */
+    public Speler meldAan(String gebruikersnaam, String wachtwoord) 
+    {
+        return null;       
+    }
+    /**
+     * 
+     * 
      * @param gebruikersnaam
      * @param wachtwoord
      */
