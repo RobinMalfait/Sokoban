ce5009208e8601f47e48f4f7bea645f4f1fb6353
Robin Malfait <malfait.robin@gmail.com>
2015-02-25 09:49:07 +0100

    Fixing warnings.

--- src/languages/LanguageManager.java
+++ src/languages/LanguageManager.java
@@ -7,52 +7,52 @@ import java.util.List;
  *
  * @author robin
  */
-public class LanguageManager 
+public class LanguageManager
 {
+
     private Language language;
-    
+
     private final List<Language> languages;
 
     public LanguageManager()
     {
         this.languages = new ArrayList<>();
     }
-    
+
     public void addLanguage(Language lang)
     {
         this.languages.add(lang);
     }
-    
+
     public String getKeuzes()
     {
         String output = "";
-        
-        for (Language language : languages)
+
+        for (Language lang : languages)
         {
-            output += ", " + language.getClass().getSimpleName();
+            output += ", " + lang.getClass().getSimpleName();
         }
-        
+
         return output.substring(2);
     }
-    
+
     public boolean setLanguage(String newLanguage)
     {
-        for (Language language : languages)
+        for (Language lang : languages)
         {
             if (language.getClass().getSimpleName().equals(newLanguage))
             {
-                this.language = language;
+                this.language = lang;
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     public String get(String index)
     {
         return this.language.get(index);
     }
-    
-    
+
 }
