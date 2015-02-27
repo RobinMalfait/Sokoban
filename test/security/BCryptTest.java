be16014ceed29cd1b6e06dce8417ca63d2aa3d6d
Robin Malfait <malfait.robin@gmail.com>
2015-02-27 22:54:16 +0100

    Adding tests for bcrypt!

---
+++ test/security/BCryptTest.java
@@ -0,0 +1,28 @@
+package security;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author robin
+ */
+public class BCryptTest
+{
+    private final String password = "foo";
+    
+    private final String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+
+    @Test
+    public void it_shoud_hash_a_password()
+    {
+        assertEquals(false, (this.password == null ? this.hashed == null : this.password.equals(this.hashed)));
+    }
+    
+    @Test
+    public void it_should_check_a_hashed_passowrd()
+    {
+        assertEquals(true, BCrypt.checkpw(this.password, this.hashed));
+    }
+}
