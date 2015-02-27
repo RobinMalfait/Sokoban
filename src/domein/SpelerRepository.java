a41c5339250e062cd0c55bb60e42130e517d2775
Bram Lemenu <bram.lemenu@me.com>
2015-02-27 19:05:41 +0100

    uitwerking methode voegToe in SpelerRepository

--- src/domein/SpelerRepository.java
+++ src/domein/SpelerRepository.java
@@ -1,3 +1,4 @@
+
 package domein;
 
 import java.sql.SQLException;
@@ -6,38 +7,63 @@ import persistentie.SpelerMapper;
 
 public class SpelerRepository
 {
-    private final SpelerMapper spelerMapper; 
+    
+    private final SpelerMapper spelerMapper;
     private List<Speler> spelers;
 
+    /**
+     * 
+     */
     public SpelerRepository()
     {
         this.spelerMapper = new SpelerMapper();
     }
 
     /**
-     * 
-     * 
+     *
+     *
      * @param gebruikersnaam
      * @param wachtwoord
+     * @return
      */
     public Speler zoekSpelerViaGebruikersnaamWachtwoord(String gebruikersnaam, String wachtwoord)
     {
         // Speler zoeken uit de SpelerMapper
         Speler speler = spelerMapper.geefSpeler(gebruikersnaam);
-        
+
         // Controleer of het wachtwoord van de speler overeenkomt met die uit de parameter
-        if (speler.getWachtwoord().equals(wachtwoord)) {
+        if (speler.getWachtwoord().equals(wachtwoord))
+        {
             return speler;
-        }
-        else {
+        } else
+        {
             return null;
         }
     }
-    
+
+    /**
+     * 
+     * @param speler 
+     */
     public void voegToe(Speler speler)
     {
-        // Add speler in Mapper
+        if (bestaatSpeler(speler.getGebruikersnaam()))
+        {
+            throw new GebruikerBestaatException("De gebruikersnaam is al in gebruik.");
+        }
+
+        spelerMapper.addSpeler(speler);
         spelers.add(speler);
     }
+    
+    /**
+     * 
+     * @param gebruikersnaam
+     * @return 
+     */
 
+    private boolean bestaatSpeler(String gebruikersnaam)
+    {
+        return spelerMapper.geefSpeler(gebruikersnaam) != null;
+    }
 }
