e2a33b39d70f45ccdec24d5a803c7f7d2a21c70e
Demian Dekoninck <demian@live.be>
2015-03-10 10:10:55 +0100

    Mogelijk gemaakt op de Spellen op te halen en weer te geven.

--- src/gui/speelSpelApplicatie.java
+++ src/gui/speelSpelApplicatie.java
@@ -6,6 +6,7 @@
 package gui;
 
 import domein.DomeinController;
+import java.util.Arrays;
 import java.util.Scanner;
 import languages.LanguageManager;
 
@@ -21,7 +22,12 @@ public class speelSpelApplicatie
         System.out.printf("Welkom %s%n", dc.geefHuidigeSpeler()[0]);
         System.out.printf("U zult nu een spel spelen.%n");
         
-        dc.speelSpel();
-        System.out.printf("%s%n", dc.toonSpelbord());
+        System.out.printf("%nKies een spel uit de lijst hieronder:%n");
+        
+        for(String[] spelString: dc.geefSpellenString())
+        {
+            System.out.printf("%10s%20s%n", spelString[0], spelString[1]);
+        }
+
     }    
 }
