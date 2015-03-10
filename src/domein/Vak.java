c079533927780c660e137a1d02c07f8ec088cc79
Demian Dekoninck <demian@live.be>
2015-03-10 11:36:38 +0100

    Attributen , getters and setters voor Vak toegevoegd volgens DCD

--- src/domein/Vak.java
+++ src/domein/Vak.java
@@ -14,6 +14,11 @@ public class Vak
     private final int posX;
     private final int posY;
 
+    private boolean doel;
+    private boolean toegankelijk;
+    
+    private Kist kist;
+    
     public Vak(int posX, int posY)
     {
         this.posX = posX;
@@ -29,6 +34,39 @@ public class Vak
     {
         return posY;
     }
+
+    public boolean isDoel()
+    {
+        return doel;
+    }
+
+    public void setDoel(boolean doel)
+    {
+        this.doel = doel;
+    }
+
+    public boolean isToegankelijk()
+    {
+        return toegankelijk;
+    }
+
+    public void setToegankelijk(boolean toegankelijk)
+    {
+        this.toegankelijk = toegankelijk;
+    }
+
+    public Kist getKist()
+    {
+        return kist;
+    }
+
+    public void setKist(Kist kist)
+    {
+        this.kist = kist;
+    }
+    
+    
+    
     
     
 }
