fc7d23596e80f3473d6ca2a813d41909f0a8268b
Demian Dekoninck <demian@live.be>
2015-03-14 13:12:15 +0100

    Mogelijk gemaakt om te stoppen, controle op foute invoer.

--- src/gui/MeldAanApplicatie.java
+++ src/gui/MeldAanApplicatie.java
@@ -1,6 +1,7 @@
 package gui;
 
 import domein.DomeinController;
+import exceptions.WachtwoordException;
 import java.util.Scanner;
 import languages.LanguageManager;
 
@@ -12,22 +13,42 @@ public class MeldAanApplicatie
 {
     public void start(DomeinController dc, Scanner input, LanguageManager lang)
     {   
+        boolean invoerFout = true;
+        boolean stop = false;
         String gebruikersnaam, wachtwoord;
         
-        //inloggegevens
-        System.out.print(lang.get("user.username") + "*: ");
-        gebruikersnaam = input.next().trim();
-
-        System.out.print(lang.get("user.password") + "*: ");
-        wachtwoord = input.next().trim();
-        
         //controle inloggegevens
-        dc.meldAan(gebruikersnaam, wachtwoord);
+        do 
+        {
+            try 
+            {
+                //inloggegevens
+                System.out.print(lang.get("user.username") + "*: ");
+                gebruikersnaam = input.next().trim();
+
+                if(gebruikersnaam.equals("stop"))
+                    stop = true;
+                else
+                {
+                    // Gebruikersnaam is niet gelijk aan "stop", dus we gaan verder.
+                    System.out.print(lang.get("user.password") + "*: ");
+                    wachtwoord = input.next().trim();
+                    
+                    dc.meldAan(gebruikersnaam, wachtwoord);
+                    invoerFout = false;                        
+                }              
+            }
+            catch(WachtwoordException e)
+            {
+                System.out.printf("%n%s%nProbeer opnieuw, op typ: stop%n%n", e.getMessage());
+            }
+        }
+        while(invoerFout && !stop);
         
-        System.out.printf("%n%s%n%n",lang.get("user.logged.in"));
-        
-        new SpeelSpelApplicatie().start(dc, input, lang); 
-        
-        
+        if(!stop) {
+            System.out.printf("%n%s%n%n",lang.get("user.logged.in"));
+            new SpeelSpelApplicatie().start(dc, input, lang);           
+        }
+ 
     }
 }
