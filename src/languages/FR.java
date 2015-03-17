2c5f004053df4ca0d87ab16a678959b12501ac9c
Robin Malfait <malfait.robin@gmail.com>
2015-03-17 09:36:59 +0100

    Cleaning languages

--- src/languages/FR.java
+++ src/languages/FR.java
@@ -4,14 +4,14 @@ package languages;
  *
  * @author robin
  */
-public class FR extends Language 
+public class FR extends Language
 {
     /**
      * Maak een nieuw FR-object aan
      */
     public FR()
     {
-        map("registration.new", "Registration pour le nouveaux joueur:");
+        map("registration.new",         "Registration pour le nouveaux joueur:");
         map("user.username",            "nom d'utilisateur");
         map("user.name",                "nom");
         map("user.firstname",           "prénom");
@@ -22,22 +22,22 @@ public class FR extends Language
         map("user.logged.out",          "déconnecté");
         map("credentials.wrong",        "mal informations d'identification");
         map("sign.in",                  "se connecter");
-        map("sign.up",                  "s'inscrire"); 
-         map("new.player", "Nouveaux joueur ajouté");
-        map("game.welcome", "Bienvenue");
-        map("game.play", "Vous aller joué un nouveaux jeu");
-        map("game.choose.list", "Choisissez un jeu dans la liste ci-dessous");
-        map("game.choose", "Choisissez un jeu");
-        map("game.playboard.load", "Le plateau de jeu est en cours de chargement");
-         map("dude.move", "déplacer le mec dans une direction");
-        map("dude.up", "en haut");
-        map("dude.down", "en bas");
-        map("dude.left", "a gauche");
-        map("dude.right", "a droite");
-        map("dude.stop", "arrêtez");
-        map("dude.my.choise", "mon chois");
-        map("game.complete", "Félicitations! vous avez terminé le jeu");
-        map("game.done","jeu terminé");
+        map("sign.up",                  "s'inscrire");
+         map("new.player",              "Nouveaux joueur ajouté");
+        map("game.welcome",             "Bienvenue");
+        map("game.play",                "Vous aller joué un nouveaux jeu");
+        map("game.choose.list",         "Choisissez un jeu dans la liste ci-dessous");
+        map("game.choose",              "Choisissez un jeu");
+        map("game.playboard.load",      "Le plateau de jeu est en cours de chargement");
+         map("dude.move",               "déplacer le mec dans une direction");
+        map("dude.up",                  "en haut");
+        map("dude.down",                "en bas");
+        map("dude.left",                "a gauche");
+        map("dude.right",               "a droite");
+        map("dude.stop",                "arrêtez");
+        map("dude.my.choise",           "mon chois");
+        map("game.complete",            "Félicitations! vous avez terminé le jeu");
+        map("game.done",                "jeu terminé");
     }
 }
 
--- src/languages/NL.java
+++ src/languages/NL.java
@@ -4,14 +4,14 @@ package languages;
  *
  * @author robin
  */
-public class NL extends Language 
+public class NL extends Language
 {
     /**
      * Maak een nieuw NL-object aan
      */
     public NL()
     {
-        map("registration.new", "Registratie voor een nieuwe speler:");
+        map("registration.new",         "Registratie voor een nieuwe speler:");
         map("user.username",            "gebruikersnaam");
         map("user.name",                "naam");
         map("user.firstname",           "voornaam");
@@ -23,21 +23,20 @@ public class NL extends Language
         map("credentials.wrong",        "foute gegevens");
         map("sign.in",                  "aanmelden");
         map("sign.up",                  "registreren");
-        map("new.player", "Nieuwe speler toegevoegd:");
-        map("game.welcome", "welkom");
-        map("game.play", "U zult een nieuw spel spelen.");
-        map("game.choose.list", "kies een spel uit onderstaande lijst");
-        map("game.choose", "Kies een spel");
-        map("game.playboard.load", "Het spelbord wordt geladen");
-        map("dude.move", "Verplaats het mannetje in een richting");
-        map("dude.up", "omhoog");
-        map("dude.down", "omlaag");
-        map("dude.left", "links");
-        map("dude.right", "rechts");
-        map("dude.stop", "stop");
-        map("dude.my.choise", "mijn keuze");
-        map("game.complete", "gefeliciteerd! je hebt het spel beïndigd.");
-            map("game.done","spel compleet");
-    
+        map("new.player",               "Nieuwe speler toegevoegd:");
+        map("game.welcome",             "welkom");
+        map("game.play",                "U zult een nieuw spel spelen.");
+        map("game.choose.list",         "kies een spel uit onderstaande lijst");
+        map("game.choose",              "Kies een spel");
+        map("game.playboard.load",      "Het spelbord wordt geladen");
+        map("dude.move",                "Verplaats het mannetje in een richting");
+        map("dude.up",                  "omhoog");
+        map("dude.down",                "omlaag");
+        map("dude.left",                "links");
+        map("dude.right",               "rechts");
+        map("dude.stop",                "stop");
+        map("dude.my.choise",           "mijn keuze");
+        map("game.complete",            "gefeliciteerd! je hebt het spel beïndigd.");
+        map("game.done",                "spel compleet");
     }
 }
